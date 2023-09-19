fa7916a7a90a7ff36f988f50bfea10476bcfb6d8
Phoenix <psstout@gmail.com>
2023-09-19 16:44:21 -0700

    can add new messages

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
+
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date(),
+    added: new Date().toDateString(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date(),
+    added: new Date().toDateString(),
   },
 ];
 
 router.get("/", (req, res) => {
-  res.render("index", { messages: messages, title: 'Mini Messageboard' });
+  res.render("index", { messages: messages, title: "Mini Messageboard" });
 });
 
-router.get("/new", (req, res) => {});
+router.get("/new", (req, res) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.content,
+    user: req.body.user,
+    added: new Date().toDateString(),
+  })
+  res.redirect('/')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+
 const app = express();
+
+// for parsing post json
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
 
 app.set("view engine", "pug");
 
 const router = require('./routes/index.js')
-app.use('/', router)
 app.listen(3000); // Port 3000
+app.use('/', router)
 
