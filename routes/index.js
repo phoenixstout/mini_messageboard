f3ada80483ffaf856b9b169b1990c124a88539c6
Phoenix <psstout@gmail.com>
2023-09-19 14:52:18 -0700

    add routing and show messages

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,23 @@
-const express = require('express')
-const router = express.Router
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", (req, res) => {
+  res.render("index", { messages: messages, title: 'Mini Messageboard' });
+});
+
+router.get("/new", (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 
+const router = require('./routes/index.js')
+app.use('/', router)
 app.listen(3000); // Port 3000
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
